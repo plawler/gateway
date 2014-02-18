2e98ab18793cfd0be46f8e26b5e86ab51d8eec31
plawler <paul.lawler@gmail.com>
2014-02-18 16:44:49 -0500

    fixed missing dependencies i removed in last merge conflict. also removed unused params from token generators

--- gateway/gateway-core/src/main/java/com/sample/gateway/core/service/ApplicationServiceHandler.java
+++ gateway/gateway-core/src/main/java/com/sample/gateway/core/service/ApplicationServiceHandler.java
@@ -2,6 +2,7 @@ package com.sample.gateway.core.service;
 
 import com.sample.gateway.core.domain.Application;
 import com.sample.gateway.core.event.*;
+import com.sample.gateway.persistence.repository.ApplicationRepository;
 import com.sample.gateway.persistence.service.ApplicationPersistenceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,6 @@ import java.util.UUID;
 @Service
 class ApplicationServiceHandler implements ApplicationService {
 
-
     private static final int CLIENT_ID_LENGTH = 10;
     private static final int CLIENT_SECRET_LENGTH = 48;
     private static final char[] DEFAULT_CODEC = "1234567890abcdefghijklmnopqrstuvwxyz".toCharArray();
@@ -30,10 +30,13 @@ class ApplicationServiceHandler implements ApplicationService {
     @Autowired
     private ApplicationPersistenceService applicationPersistenceService;
 
+    @Autowired
+    private ApplicationRepository applicationRepository;
+
     @Override
     public RegisteredApplicationEvent registerNewApplication(RegisterApplicationEvent registerApplicationEvent) {
         Application application = Application.fromApplicationData(registerApplicationEvent.getData());
-        application.approve(generateClientId(application), generateSharedSecret(application));
+        application.approve(generateClientId(), generateSharedSecret());
         return applicationPersistenceService.registerApplication(new RegisterApplicationEvent(application.details()));
     }
 
@@ -49,12 +52,44 @@ class ApplicationServiceHandler implements ApplicationService {
         return applicationPersistenceService.modifyApplication(new ModifyApplicationEvent(application.details()));
     }
 
-    private String generateSharedSecret(Application application) {
-        return UUID.randomUUID().toString();
+    private String generateSharedSecret() {
+        return generateToken(CLIENT_SECRET_LENGTH);
     }
 
-    private String generateClientId(Application application) {
-        return application.getApplicationName().replace(" ", "") + "123456";
+    private String generateClientId() {
+        String clientId = generateToken(CLIENT_ID_LENGTH);
+        while(applicationRepository.findByClientId(clientId)!=null) // TODO: revisit
+            clientId = generateToken(CLIENT_ID_LENGTH);
+        return clientId;
     }
+
+    /**straight up copy-and-pasted from TokenGrabber.java in SDS project**/
+
+    /**
+     * Generates a random string containing characters a-z, A-Z, 0-9
+     *
+     * @param length length of string
+     * @return a securely random string
+     */
+    public static String generateToken(int length) {
+        byte[] verifierBytes = new byte[length];
+        random.nextBytes(verifierBytes);
+        return getAuthorizationCodeString(verifierBytes);
+    }
+
+    /**
+     * Grabbed this out of org.springframework.security.oauth2.provider.code.RandomValueAuthorizationCodeServices
+     *
+     * @param verifierBytes The bytes.
+     * @return The string.
+     */
+    private static String getAuthorizationCodeString(byte[] verifierBytes) {
+        char[] chars = new char[verifierBytes.length];
+        for (int i = 0; i < verifierBytes.length; i++) {
+            chars[i] = DEFAULT_CODEC[((verifierBytes[i] & 0xFF) % DEFAULT_CODEC.length)];
+        }
+        return new String(chars);
+    }
+
 
 }
