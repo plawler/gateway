74f0576da6bb930a6a7405ebf4f1340a5b6ab195
plawler <paul.lawler@gmail.com>
2014-03-20 16:39:07 -0400

    initial buildout of the portal web app for managing app development in a multi-operator environment

---
+++ gateway/gateway-portal/src/main/java/com/inbloom/portal/configuration/WebConfig.java
@@ -0,0 +1,48 @@
+package com.inbloom.portal.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+/**
+ * Created By: paullawler
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public TemplateResolver templateResolver() {
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setOrder(1);
+        return resolver;
+    }
+
+}
---
+++ gateway/gateway-portal/src/main/java/com/inbloom/portal/web/HelloController.java
@@ -0,0 +1,20 @@
+package com.inbloom.portal.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created By: paullawler
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hi there, World!");
+        return "hello";
+    }
+
+}
