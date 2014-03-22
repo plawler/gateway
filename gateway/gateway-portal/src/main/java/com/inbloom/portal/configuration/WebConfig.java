e6ded6874996cc8e5d401d4f41c6a2085a273cf6
plawler <paul.lawler@gmail.com>
2014-03-22 14:42:15 -0400

    added layout and provided a content example

--- gateway/gateway-portal/src/main/java/com/inbloom/portal/configuration/WebConfig.java
+++ gateway/gateway-portal/src/main/java/com/inbloom/portal/configuration/WebConfig.java
@@ -11,9 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 
 }
--- gateway/gateway-portal/src/main/webapp/WEB-INF/views/hello.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-	<h1>${message}</h1>
-    <p>Just hangin' around in case you want to go old school JSTL and whatnot.</p>
-</body>
-</html>
