a3dbcfecfb22f810b2da23a140b21fc25ccf2a37
benjamin-edward-morgan <benjamin.morgan@inbloom.org>
2014-04-04 15:59:40 -0400

    stubbed out some error handling

--- gateway/gateway-portal/src/main/java/org/inbloom/portal/web/VerifyEmailController.java
+++ gateway/gateway-portal/src/main/java/org/inbloom/portal/web/VerifyEmailController.java
@@ -19,18 +19,30 @@ public class VerifyEmailController {
     @RequestMapping(method=RequestMethod.GET)
     public String get(Model model, @RequestParam("token") String token) {
 
-
         model.addAttribute("token", token);
+
+        //TODO: check that token is valid
+        if(false) {
+            model.addAttribute("message", "That token is not valid!");
+            return "error";
+        }
 
         return "setPassword";
     }
 
 
     @RequestMapping(method=RequestMethod.POST)
-    public String post(Model model, @ModelAttribute SignupCompletion passwordAndToken)
-    {
+    public String post(Model model, @ModelAttribute SignupCompletion passwordAndToken) {
         System.out.println(passwordAndToken.getPassword());
         System.out.println(passwordAndToken.getToken());
+
+        //TODO: post password and token to api
+        //on fail, reload page with error msg
+        if(false) {
+            model.addAttribute("errorMessage", "Danger danger!!!");
+            model.addAttribute("token", passwordAndToken.getToken());
+            return "setPassword";
+        }
 
         return "hello";
     }
