dae325c89d821849a433cf74043768067f002b79
Bill Siggelkow <bill.siggelkow@inbloom.org>
2014-04-09 15:02:10 -0400

    Move LDAPCleanup after hook to hooks.rb

--- acceptance-tests/features/step_definitions/common.rb
+++ acceptance-tests/features/step_definitions/common.rb
@@ -65,37 +65,3 @@ def account_validation_with_an_invalid_password_resource
   }
 end
 
-After '@LDAPCleanup' do
-  ldap_cleanup
-end
-
-def ldap_cleanup
-  base = 'ou=LocalNew,ou=DevTest,dc=slidev,dc=org'
-  group_base  = "ou=groups,#{base}"
-
-  ldap_config = {
-    :host => '127.0.0.1',
-    :port => 10389,
-    :base => group_base, # sets the treebase for search
-    :auth => {
-      :method => :simple,
-      :username => 'cn=Admin,dc=slidev,dc=org',
-      :password => 'test1234'
-    }
-  }
-
-  Net::LDAP.open(ldap_config) do |ldap|
-    # Remove user from the groups
-    ['application_developer', 'Sandbox Administrator'].each do |group_id|
-      group_dn = "cn=#{group_id},#{group_base}"
-      group = ldap.search(:filter => Net::LDAP::Filter.eq('cn', group_id)).first
-      if group
-        removed = group[:memberuid].delete(@app_provider_email)
-        ldap.replace_attribute(group_dn, :memberuid, group[:memberuid]) if removed
-      end
-    end
-
-    # Delete the user
-    ldap.delete(:dn => "cn=#{@app_provider_email},ou=people,#{base}")
-  end
-end
