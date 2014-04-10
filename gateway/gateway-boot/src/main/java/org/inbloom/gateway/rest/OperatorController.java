7dfdc2e1cdc9ba6fb953ae5487e280a3b203cc6a
Verlin Henderson <verlin.henderson@inbloom.org>
2014-04-10 14:04:54 -0400

    Removed bad imports

--- gateway/gateway-boot/src/main/java/org/inbloom/gateway/rest/OperatorController.java
+++ gateway/gateway-boot/src/main/java/org/inbloom/gateway/rest/OperatorController.java
@@ -6,8 +6,6 @@ import com.wordnik.swagger.annotations.ApiErrors;
 import com.wordnik.swagger.annotations.ApiOperation;
 
 import org.inbloom.gateway.common.domain.Operator;
-import org.inbloom.gateway.common.status.rest.StatusResponse;
-import org.inbloom.gateway.core.domain.Operator;
 import org.inbloom.gateway.core.event.operator.*;
 import org.inbloom.gateway.core.service.OperatorService;
 import org.inbloom.gateway.common.status.OperatorStatus;
